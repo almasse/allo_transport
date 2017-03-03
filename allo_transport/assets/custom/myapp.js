4fcb5e9299947aaa186b74ad369290e2cf7bfb9f
programmeur2 <apoirier@d-modules.com>
2017-03-03 16:16:37 -0500

    details update

--- allo_transport/assets/custom/myapp.js
+++ allo_transport/assets/custom/myapp.js
@@ -1,18 +1,79 @@
-$(document).ready(function () {
 
-    $.getJSON("http://localhost:8000/api/v2/pages/4/?format=json", function (page_data) {
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+function renderNews(id){
+
+    $.getJSON("http://localhost:8000/api/v2/pages/"+id+"/?format=json", function (page_data) {
+
+        var template = $('#template-news').html();
+        var rendered = Mustache.render(template, page_data);
         
-        console.log(page_data); 
+        $('#news').html(rendered);
+
+    });
+}
+
+function renderIndex(){
+
+    var newspagesids = [];
+    var latestsnewsids = [];
+    $.getJSON("http://localhost:8000/api/v2/pages/4/?format=json", function (page_data) {
+
         var template = $('#template-partners').html();
         var rendered = Mustache.render(template, page_data);
-        console.log(rendered);
+        
         $('#partners').html(rendered);
 
     });
+    $.getJSON("http://localhost:8000/api/v2/pages/?format=json", function (data) {
+        
+        for (var key in data.items){
+            if (data.items[key].meta.type == "allo_transport.NewsPage"){
+                newspagesids.push(data.items[key].id);
+            }
+        }
 
+        newspagesids.sort(function(a,b){
+            return b-a;
+        });
+        console.log(newspagesids);
 
-});
+        var count = 0;
+        for (var id in newspagesids){
+            
+            if (count == 4){
+                break;
+            }
+            count = count + 1 ;
+            latestsnewsids.push(newspagesids[id]);
+        }
+        console.log(latestsnewsids);
+        for (var ids in latestsnewsids){
+            $.getJSON("http://localhost:8000/api/v2/pages/"+latestsnewsids[ids]+"/?format=json", function(page){
 
+                var template = $('#template-news').html();
+                var rendered = Mustache.render(template, page);
+                $('#news').append(rendered);
+            });
+        }
+        //var template = $('#template-partners').html();
+        //var rendered = Mustache.render(template, page_data);
+        //console.log(rendered);
+        //$('#partners').html(rendered);
 
-
+    });
+}
 
--- allo_transport_api/apps/allo_transport/models.py
+++ allo_transport_api/apps/allo_transport/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
 from django.conf import settings
 
 
 class HomePage(Page):
-    api_fields = ['partners']
+    api_fields = ['partners', 'subpage_types']
 
     content_panels = Page.content_panels + [
         InlinePanel('partners', label="Partenaires"),
     ]
-    
+    subpage_types = ['allo_transport.NewsPage']
+
+
 class Partner(Orderable):
     page = ParentalKey(HomePage, related_name='partners')
     url = models.CharField(max_length=255)
@@ -34,3 +37,71 @@ class Partner(Orderable):
     def photo_url(self):
         if self.photo:
             return settings.MEDIA_URL + 'original_images/' + self.photo.filename
+
+
+class NewsPage(Page):
+    body = RichTextField(blank=True)
+    apercu = models.CharField(max_length=150,blank=True)
+    photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    api_fields = ['body','photo','photo_url','apercu']
+    
+
+
+    parent_page_types = ['allo_transport.HomePage']
+    subpage_types = []
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+        FieldPanel('photo'),
+        FieldPanel('apercu'),
+    ]
+
+    @property
+    def photo_url(self):
+        if self.photo:
+            return settings.MEDIA_URL + 'original_images/' + self.photo.filename
+
+"""
+class Photo(Orderable):
+    page = ParentalKey(NewsPage, related_name='photo')
+    url = models.CharField(max_length=255)
+    photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    api_fields = ['url', 'photo', 'photo_url']
+
+    panels = [
+        FieldPanel('url'),
+        FieldPanel('photo'),
+    ]
+
+    @property
+    def photo_url(self):
+        if self.photo:
+            return settings.MEDIA_URL + 'original_images/' + self.photo.filename
+
+
+class Content(Orderable):
+    page = ParentalKey(NewsPage, related_name='content')
+    title = models.CharField(max_length=255,null=True,blank=True)
+    description = RichTextField(null=True,blank=True)
+
+    api_fields = ['title', 'description']
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('description'),
+    ]
+"""
